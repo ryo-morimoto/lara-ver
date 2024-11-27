4fc296cb2b31aaed15b42c116d0a40f936fc8477
ryo-morimoto <86156534+ryo-morimoto@users.noreply.github.com>
2024-11-27 23:24:22 +0900

    manifesto.jsonで拡張機能のnameとdescriptionを設定

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,4 +5,9 @@ export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   srcDir: 'src',
+  manifest: {
+    name: 'Lara Ver - ドキュメントのバージョンを自動固定',
+    version: '1.0.0',
+    description: '⚡️ Laravelドキュメントのバージョン切り替えを自動化！ 📌 お気に入りのバージョンを固定して、快適なドキュメント閲覧を実現します ✨ 開発効率アップをサポートする Chrome 拡張機能です。',
+  },
 });
